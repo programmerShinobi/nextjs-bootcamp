1014b4d7959951411823def2d6ab0632bb3e20e1
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-01-30 08:02:22 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React,{useEffect} from "react";
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import { Provider } from 'react-redux';
 import store  from '../Redux/Store';
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  
+  useEffect(() => {
+    if (router.pathname === '/') {
+      router.push('/users');
+    }
+  });
+  
   return (
     <Provider store={store}>
     <Layout>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </Layout>
     </Provider>
   )
