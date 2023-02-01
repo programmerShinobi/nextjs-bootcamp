ce61e986c3165600c2872febeb7a65a50552ffcd
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-02-01 10:56:23 +0700

    Update nextjs

--- components/AuthLayout.js
+++ components/AuthLayout.js
@@ -5,13 +5,13 @@ export default function AuthLayout({ children }) {
     return (
         <>
             <div className="flex h-screen bg-gray-700">
-                <div className="m-auto bg-slate-50 rounded-xl w-3/5 h-3/4 grid lg:grid-cols-2 ">
+                <div className="m-auto rounded-xl w-3/5 h-3/4 grid lg:grid-cols-2 ">
                     <div className={styles.imgStyle}>
                         <div className={styles.cartoonImg}></div>
                         <div className={styles.shuriken_one}></div>
                         <div className={styles.shuriken_two}></div>
                     </div>
-                    <div className="right flex flex-col justify-evenly">
+                    <div className="right flex flex-col bg-slate-50 form-auth">
                         <div className="text-center py-10">
                             {children}
                         </div>
