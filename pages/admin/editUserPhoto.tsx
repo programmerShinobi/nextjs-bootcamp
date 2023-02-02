49ed887615d020c84df909d7a7c02ce794d5da44
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-02-02 23:18:56 +0700

    Update editUserPhoto.tsx

--- pages/admin/editUserPhoto.tsx
+++ pages/admin/editUserPhoto.tsx
@@ -89,7 +89,7 @@ const EditUserPhoto: NextPage<Props> = ({ dirs })=> {
 export const getServerSideProps: GetServerSideProps = async () => {
   const props = { dirs: [] };
   try {
-    const dirs = await fs.readdir(path.join(process.cwd(), "/public/images"));
+    const dirs = await fs.readdir(path.join(process.cwd(), "../../public/images"));
     props.dirs = dirs as any;
     return { props };
   } catch (error) {
